c316e69d0c001561cafc448acd2a12e4f38d4938
jorismeurs <jorismeurs@gmail.com>
2020-06-04 09:38:06 +0200

    Updated MVA part
    PCA added
    Subplot of scores and loadings per component or endmember

--- SIMSimzML.m
+++ SIMSimzML.m
@@ -11,7 +11,7 @@ classdef SIMSimzML < readimzML & extractFeatures & customisePlot
     % - Statistics & Machine Learning Toolbox
     
     properties 
-        version = '0.0.3'
+        version = '0.0.5'
         developer = 'Joris Meurs, MSc'
         matlabVersion = 'R2017a'
         dependencies = {'Bioinformatics Toolbox','Statistics & Machine Learning Toolbox'}
@@ -78,19 +78,55 @@ classdef SIMSimzML < readimzML & extractFeatures & customisePlot
                 warning('Normalising intensities first');
                 obj = normalise(obj);
             end
-
-            rng(1);
-            [W,H] = nnmf(obj.normalisedIntensity,obj.components);
-            for j = 1:obj.components
-                f = figure;
-                endmemberImage = reshape(W(:,j),obj.pixelRows,obj.pixelColumns);
-                colormap(obj.CMAP);
-                imagesc(endmemberImage)
-                colorbar();
-                title(sprintf('Endmember %d',j));
-                xlabel(obj.XLabel);
-                ylabel(obj.XLabel);
-                set(gcf,'Color','white');
+            if isequal(obj.mvaType,'NMF')
+                rng(1);
+                [W,H] = nnmf(obj.normalisedIntensity',obj.components);
+                for j = 1:obj.components
+                    figure;
+                    endmemberImage = reshape(W(:,j),obj.pixelRows,obj.pixelColumns);                   
+                    subplot(1,2,1)
+                    colormap(obj.CMAP);
+                    imagesc(endmemberImage)
+                    colorbar();
+                    title(sprintf('Endmember %d',j));
+                    xlabel(obj.XLabel);
+                    ylabel(obj.XLabel);
+                    axis square
+                    set(gca,'FontName',obj.fontName);
+                    
+                    subplot(1,2,2)
+                    stem(obj.uniqueFeatures{obj.selectedFile},H(j,:),'Marker','none','Color','b');
+                    xlabel('m/z');
+                    ylabel('H');
+                    set(gca,'FontName',obj.fontName);
+                    
+                    set(gcf,'Color','white');
+                end
+            elseif isequal(obj.mvaType,'PCA')
+                [loadings,scores,~,~,explained] = pca(obj.normalisedIntensity');
+                
+                for j = 1:obj.components
+                    figure;
+                    subplot(1,2,1) % Scores
+                    colormap(obj.CMAP);
+                    pcaImage = reshape(scores(:,j),obj.pixelRows,obj.pixelColumns);
+                    imagesc(pcaImage);
+                    xlabel(obj.XLabel);
+                    ylabel(obj.YLabel);
+                    title(sprintf('PC%d (%.1f%%)',j,explained(j)));
+                    set(gca,'FontName',obj.fontName);
+                    axis square
+                                        
+                    subplot(1,2,2) % Loadings
+                    stem(obj.uniqueFeatures{obj.selectedFile},loadings(:,j),'Marker','none','Color','b');
+                    xlabel('m/z');
+                    ylabel(sprintf('Loadings PC%d',j));
+                    set(gca,'FontName',obj.fontName);
+                    set(gcf,'Color','white');
+                end
+                
+            else
+               error(sprintf('MVA type %s not supported',obj.mvaType)); 
             end
         end
     end
