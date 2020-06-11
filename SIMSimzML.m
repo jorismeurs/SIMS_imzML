1b7a2d0adc1a5f2708617813a9220731db3d6a0b
jorismeurs <jorismeurs@gmail.com>
2020-06-11 13:36:29 +0200

    Multiple plots updated

--- SIMSimzML.m
+++ SIMSimzML.m
@@ -9,10 +9,10 @@ classdef SIMSimzML < readimzML & customisePlot
     % - Statistics & Machine Learning Toolbox
     
     properties 
-        version = '0.0.6'
+        version = '0.0.7'
         developer = 'Joris Meurs, MSc'
         matlabVersion = 'R2017a'
-        dependencies = {'Bioinformatics Toolbox','Statistics & Machine Learning Toolbox'}
+        dependencies = {'Bioinformatics Toolbox'}
     end
     
     properties
@@ -27,6 +27,7 @@ classdef SIMSimzML < readimzML & customisePlot
             obj.options.plotimages = 'all';
             obj.options.saveimage = 'true';
             obj.options.exportformat = 'tif';
+            obj.options.tolerance = 0.01;
         end
         
         function obj = selectFile(obj)
@@ -45,85 +46,74 @@ classdef SIMSimzML < readimzML & customisePlot
         function obj = ionImage(obj)
            clc
            if isempty(obj.mz)
-               warning('No m/z value selected. Executing selectedMZ');
-               obj = selectMZ(obj);
+               warning('No m/z value selected');
+               return
            end
-           try
-               if isequal(obj.options.plotimages,'single')
-                   spectralData = obj.spectra{obj.file};
-                   fileTIC = cell2mat(obj.totIonCount{obj.file});
-                   mzInt = [];
-                   for j = 1:length(spectralData)
-                      pixelMS = cell2mat(spectralData(j,1)); 
-                      ionIDX = find(pixelMS(:,1) > obj.mz-0.001 & pixelMS(:,1) < obj.mz+0.001);
+
+           if isequal(obj.options.plotimages,'single')
+               spectralData = obj.spectra{obj.file};
+               fileTIC = cell2mat(obj.totIonCount{obj.file});
+               mzInt = [];
+               for j = 1:length(spectralData)
+                  pixelMS = cell2mat(spectralData(j,1)); 
+                  ionIDX = find(pixelMS(:,1) > obj.mz-0.001 & pixelMS(:,1) < obj.mz+0.001);
+                  if ~isempty(ionIDX)
+                     mzInt = [mzInt;sum(pixelMS(ionIDX,2))/fileTIC(j,1)]; 
+                  else
+                     mzInt = [mzInt;0]; 
+                  end
+               end
+               reconstructedIntensities = reshape(mzInt,...
+                   obj.pixelRows,obj.pixelColumns);
+               colormap(obj.CMAP);
+               imagesc(reconstructedIntensities);
+           end
+           if isequal(obj.options.plotimages,'all')
+               for j = 1:length(obj.spectra)
+                  spectralData = obj.spectra{j};
+                  fileTIC = cell2mat(obj.totIonCount{j});
+                  mzInt = [];
+                  for n = 1:length(spectralData)
+                      pixelMS = cell2mat(spectralData(n,1));
+                      ionIDX = find(pixelMS(:,1) > obj.mz-obj.options.tolerance & pixelMS(:,1) < obj.mz+obj.options.tolerance);
                       if ~isempty(ionIDX)
                          mzInt = [mzInt;sum(pixelMS(ionIDX,2))/fileTIC(j,1)]; 
                       else
                          mzInt = [mzInt;0]; 
                       end
-                   end
-                   reconstructedIntensities = reshape(mzInt,...
-                       obj.pixelRows,obj.pixelColumns);
-                   colormap(obj.CMAP);
-                   imagesc(reconstructedIntensities);
-                   xlabel(obj.XLabel);
-                   ylabel(obj.YLabel);
-                   colorbar();
-                   if ~isempty(obj.XTick)
-                        set(gca,'XTick',XTick);
-                   end
-                   if ~isempty(obj.YTick)
-                        set(gca,'YTick',YTick);
-                   end 
-                   if ~isempty(obj.XTickLabels)
-                        set(gca,'XTickLabels',num2str(XTickLabels)) 
-                   end
-                   if ~isempty(obj.YTickLabels)
-                        set(gca,'XTickLabels',num2str(XTickLabels))
-                   end
+                  end
+                  figure(j)
+                  reconstructedIntensities = reshape(mzInt,...
+                  obj.pixelRows,obj.pixelColumns);
+                  colormap(obj.CMAP);
+                  imagesc(reconstructedIntensities);
+                  setPlot(obj,j)                      
                end
-               if isequal(obj.options.plotimages,'all')
-                   for j = 1:length(obj.spectra)
-                      spectralData = obj.spectra{j};
-                      fileTIC = cell2mat(obj.totIonCount{j});
-                      mzInt = [];
-                      for n = 1:length(spectralData)
-                          pixelMS = cell2mat(spectralData(n,1));
-                          ionIDX = find(pixelMS(:,1) > obj.mz-0.001 & pixelMS(:,1) < obj.mz+0.001);
-                          if ~isempty(ionIDX)
-                             mzInt = [mzInt;sum(pixelMS(ionIDX,2))/fileTIC(j,1)]; 
-                          else
-                             mzInt = [mzInt;0]; 
-                          end
-                      end
-                      figure(j)
-                      reconstructedIntensities = reshape(mzInt,...
-                      obj.pixelRows,obj.pixelColumns);
-                      colormap(obj.CMAP);
-                      imagesc(reconstructedIntensities);
-                      xlabel(obj.XLabel);
-                      ylabel(obj.YLabel);
-                      title(obj.files{j},'interpreter','none');
-                      colorbar();
-                      if ~isempty(obj.XTick)
-                          set(gca,'XTick',XTick);
-                      end
-                      if ~isempty(obj.YTick)
-                          set(gca,'YTick',YTick);
-                      end    
-                      if ~isempty(obj.XTickLabels)
-                         set(gca,'XTickLabels',num2str(XTickLabels)) 
-                      end
-                      if ~isempty(obj.YTickLabels)
-                          set(gca,'XTickLabels',num2str(XTickLabels))
-                      end
-                   end
-               end
-           catch e
-               rethrow(e)
            end
         end
         
+        function setPlot(obj,j)
+              if nargin < 2
+                 title(obj.files{obj.file},'interpreter','none');  
+              else
+                 title(obj.files{j},'interpreter','none'); 
+              end
+              xlabel(obj.XLabel);
+              ylabel(obj.YLabel);          
+              colorbar();
+              if ~isempty(obj.XTick)
+                  set(gca,'XTick',XTick);
+              end
+              if ~isempty(obj.YTick)
+                  set(gca,'YTick',YTick);
+              end    
+              if ~isempty(obj.XTickLabels)
+                 set(gca,'XTickLabels',num2str(XTickLabels)) 
+              end
+              if ~isempty(obj.YTickLabels)
+                  set(gca,'XTickLabels',num2str(XTickLabels))
+              end
+        end
     end
     
 end
